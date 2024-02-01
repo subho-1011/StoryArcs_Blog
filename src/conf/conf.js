7637a51b1436dc3cdf31763111b3caca34a7c078
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-01 20:31:25 +0530

    conf env variable

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf = {
+    appwrite: {
+        url: String(import.meta.env.VITE_ENDPOINT),
+        projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+        databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+        collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+        bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+    }
+}
+
+export default conf
