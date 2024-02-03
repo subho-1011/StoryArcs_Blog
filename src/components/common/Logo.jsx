1182811d98f11e77c833b61cdb2f9df87016177c
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-03 11:04:13 +0530

    add simple logo

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -2,8 +2,11 @@
 // eslint-disable-next-line no-unused-vars
 const Logo = ({ width = "100px" }) => {
   return (
-    <div>Logo</div>
-  )
+    <div>
+      <span className="text-2xl font-medium text-rose-900">Story</span>
+      <span className="text-2xl font-medium text-blue-900">Arcs</span>
+    </div>
+  );
 }
 
 export default Logo
