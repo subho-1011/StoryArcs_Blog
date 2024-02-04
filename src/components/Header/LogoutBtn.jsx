ff4608933cc2552f70f76a9b5c219774046880dd
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-04 15:48:07 +0530

    return to home

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,15 +1,19 @@
 import { useDispatch } from "react-redux"
 import authService from "../../appwrite/auth"
 import { logout } from "../../store/authSlice"
+import { useNavigate } from "react-router-dom"
 
 const LogoutBtn = () => {
+  const navigate = useNavigate()
 
     const dispatch = useDispatch()
     const handleLogout = () => {
-        authService.logout().then(() => dispatch(logout()))
+        authService.logout();
+        dispatch(logout())
+        navigate("/", { replace: true })
     }
   return (
-    <button onClick={handleLogout} className="inline-block px-4 py-2 text-white duration-200 hover:bg-blue-200 rounded-full">Logout</button>
+    <button onClick={handleLogout}  className="inline-block px-4 py-2 text-white duration-200 hover:bg-blue-200 rounded-full">Logout</button>
   )
 }
 
