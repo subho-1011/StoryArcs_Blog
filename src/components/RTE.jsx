23c7936ccecf7064f2035f8ab5763e485fb64ef0
Subhajit Pramanik <subhajitpramanik1011@gmail.com>
2024-02-04 07:56:13 +0530

    add RTE APIKey

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,8 @@
 import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
+import conf from "../conf/conf";
 
-export default function RTE({ name, control, label, defaultValue = "" }) {
+export default function RTE({ name, control, label, defaultValue = "Hello World" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
@@ -12,6 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+            apiKey={conf.tinyMCEKey}
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,11 +1,12 @@
 const conf = {
-    appwrite: {
-        url: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
-        projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
-        databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-        collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
-        bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
-    }
-}
+  appwrite: {
+    url: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
+    projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+  },
+  tinyMCEKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
+};
 
-export default conf
+export default conf;
